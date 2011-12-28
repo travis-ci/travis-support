b4093cfdb33b050ee6f2cca27b1f823377cb30c6
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-12-28 18:14:13 +0100

    Changes to Async::Queue
    - Queue is not an Array, it contains an array of items/jobs. This change favors composition over inheritance.
    
    - Wrap all calls to the items array in a Mutex as the Array class is not threadsafe.

--- lib/core_ext/module/async.rb
+++ lib/core_ext/module/async.rb
@@ -1,19 +1,33 @@
 require 'thread'
 
 class Async
-  class Queue < Array
+  class Queue
     attr_reader :name
+    attr_reader :items
+
+    @@lock = Mutex.new
 
     def initialize(name)
-      @name = name
+      @name  = name
+      @items = []
       Thread.new { loop { work } }
     end
 
     def work
-      block = pop
+      block = with_lock { @items.pop }
       block.call if block
     rescue Exception => e
       puts e.message, e.backtrace
+    end
+
+    def <<(item)
+      with_lock { @items << item }
+    end
+
+    private
+
+    def with_lock
+      @@lock.synchronize { yield }
     end
   end
 
