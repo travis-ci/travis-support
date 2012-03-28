80fa0a75cd0d27b2cc69c48b81c0a02ba3ef7e38
Mathias Meyer <meyer@paperplanes.de>
2012-03-28 13:08:19 +0200

    Properly initialize exchange.

--- lib/travis/support/amqp/bunny/publisher.rb
+++ lib/travis/support/amqp/bunny/publisher.rb
@@ -24,7 +24,7 @@ module Travis
       protected
 
         def exchange
-          @exchange ||= Bunny::Exchange.new(channel, name, :type => type.to_sym, :durable => true, :auto_delete => false)
+          @exchange ||= Bunny::Exchange.new(Amqp.connection, name, :type => type.to_sym, :durable => true, :auto_delete => false)
         end
 
         def channel
