084da64fd3ddf2efa64e519e0f121bac21706fbf
Mathias Meyer <meyer@paperplanes.de>
2012-04-03 19:12:53 +0200

    Add preliminary specs for the bunny publisher.

---
+++ spec/travis/support/amqp/bunny/publisher_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Travis::Amqp::Publisher do
+  let(:connection) {Travis::Amqp.connection}
+  before do
+    Travis::Amqp.config = {
+      host: 'localhost'
+    }
+    connection
+  end
+  let(:publisher) {Travis::Amqp::Publisher.new('reporting')}
+  let(:queue) {
+    queue = connection.queue('reporting')
+    exchange = connection.exchange('reporting.jobs.1', :durable => true, :type => :topic, :auto_delete => false)
+    queue.bind(exchange, :key => 'reporting')
+    queue
+  }
+
+  let(:message) {
+    queue.pop
+  }
+
+  it "should encode the data as json" do
+    publisher.publish({})
+    message.should_not == nil
+    message[:payload].should == "{}"
+  end
+
+  it "should default to a direct type" do
+    publisher.type.should == "direct"
+  end
+
+end
