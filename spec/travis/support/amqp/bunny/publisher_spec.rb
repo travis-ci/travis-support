ef55a9ae5c97276cba18b8c0200bf931667a963a
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-04-09 12:35:15 +0200

    pre setup the queue in the spec

--- spec/travis/support/amqp/bunny/publisher_spec.rb
+++ spec/travis/support/amqp/bunny/publisher_spec.rb
@@ -6,7 +6,7 @@ if RUBY_PLATFORM != 'java'
     let(:connection) { Travis::Amqp.connection }
     let(:message)    { queue.pop(:nowait => false) }
     let(:publisher)  { Travis::Amqp::Publisher.new('reporting') }
-    let(:queue) do
+    let!(:queue) do
       queue = connection.queue('reporting', :durable => true, :exclusive => false)
       exchange = connection.exchange('reporting.jobs.1', :durable => true, :type => :topic, :auto_delete => false)
       queue.bind(exchange, :key => 'reporting')
