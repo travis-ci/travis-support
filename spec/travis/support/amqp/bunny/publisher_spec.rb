8bb57a458f717d1f4bc75c8692431100d742c5f9
Josh Kalderimis <josh.kalderimis@gmail.com>
2012-04-09 12:26:42 +0200

    minor spec changes

--- spec/travis/support/amqp/bunny/publisher_spec.rb
+++ spec/travis/support/amqp/bunny/publisher_spec.rb
@@ -1,36 +1,31 @@
 require 'spec_helper'
 
 if RUBY_PLATFORM != 'java'
+
   describe Travis::Amqp::Publisher do
-    let(:connection) {Travis::Amqp.connection}
-    before do
-      Travis::Amqp.config = {
-        :host => 'localhost'
-      }
-      connection
-      queue
-    end
-    let(:publisher) {Travis::Amqp::Publisher.new('reporting')}
-    let(:queue) {
+    let(:connection) { Travis::Amqp.connection }
+    let(:message)    { queue.pop(:nowait => false) }
+    let(:publisher)  { Travis::Amqp::Publisher.new('reporting') }
+    let(:queue) do
       queue = connection.queue('reporting', :durable => true, :exclusive => false)
       exchange = connection.exchange('reporting.jobs.1', :durable => true, :type => :topic, :auto_delete => false)
       queue.bind(exchange, :key => 'reporting')
       queue
-    }
+    end
 
-    let(:message) {
-      queue.pop(:nowait => false)
-    }
+    before do
+      Travis::Amqp.config = { :host => 'localhost' }
+    end
 
-    it "should encode the data as json" do
+    it "encodes the data as json" do
       publisher.publish({})
       message.should_not == nil
       message[:payload].should == "{}"
     end
 
-    it "should default to a direct type" do
+    it "defaults to a direct type" do
       publisher.type.should == "direct"
     end
-
   end
+
 end
