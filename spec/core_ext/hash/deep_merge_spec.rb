9f18805837314321d75ac24ca90705fcdb91b855
Sven Fuchs <svenfuchs@artweb-design.de>
2012-04-11 01:59:35 +0200

    move core_exts from travis-build to here

---
+++ spec/core_ext/hash/deep_merge_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'core_ext/hash/deep_merge'
+
+describe 'Hash#deep_merge' do
+  it 'deep merges a hash into a new one' do
+    lft = { :foo => { :bar => 'bar' } }
+    rgt = { :foo => { :baz => 'baz' } }
+    lft.deep_merge(rgt).should == { :foo => { :bar => 'bar', :baz => 'baz' } }
+  end
+
+  it 'does not change self' do
+    lft = { :foo => { :bar => 'bar' } }
+    rgt = { :foo => { :baz => 'baz' } }
+    lft.deep_merge(rgt)
+    lft.key?(:baz).should be_false
+  end
+end
+
+describe 'Hash#deep_merge!' do
+  it 'deep merges a hash into self' do
+    lft = { :foo => { :bar => 'bar' } }
+    rgt = { :foo => { :baz => 'baz' } }
+    lft.deep_merge!(rgt)
+    lft.should == { :foo => { :bar => 'bar', :baz => 'baz' } }
+  end
+end
