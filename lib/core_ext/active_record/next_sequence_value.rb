cd63b4b498d9fe7c4069af338e336293d1a4d658
Sven Fuchs <me@svenfuchs.com>
2012-09-19 23:31:40 +0200

    move next_sequence_value to here

---
+++ lib/core_ext/active_record/next_sequence_value.rb
@@ -0,0 +1,10 @@
+require 'active_record/base'
+require 'active_record/connection_adapters/abstract_adapter'
+require 'active_record/connection_adapters/postgresql_adapter'
+
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+  def next_sequence_value(sequence_name)
+    Integer(select_value("SELECT NEXTVAL('#{sequence_name}')"))
+  end
+end
+
