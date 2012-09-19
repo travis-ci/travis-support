268dc0ff74b5a559e06f637e0814af983e60919e
Sven Fuchs <me@svenfuchs.com>
2012-09-19 23:39:18 +0200

    gotta require active_record here

--- lib/core_ext/active_record/none_scope.rb
+++ lib/core_ext/active_record/none_scope.rb
@@ -1,3 +1,5 @@
+require 'active_record/base'
+
 # will be added in rails 4, doing it more properly
 # https://github.com/rails/rails/commit/75de1ce131cd39f68dbe6b68eecf2617a720a8e4
 # for now we'll just limit to zero
