fe5e9013d99f8f38b9560cbd3150146914e2c138
Piotr Sarnacki <drogus@gmail.com>
2012-12-10 13:37:48 +0100

    Revert "Don't let call default to_json and as_json functions on AR records"
    This isn't ready to use, as we still have parts of the system calling
    `to_json` without override in the model. Unfortunately those failures
    didn't come up in the tests.
    
    This reverts commit 124b8b6f6f1cff468b678a38583191a18f9db922.

--- lib/core_ext/active_record/base.rb
+++ lib/core_ext/active_record/base.rb
@@ -8,15 +8,6 @@ class ActiveRecord::Base
       'sqlite3'    => 'round(%s - 0.5)'
     }
   }
-
-  def to_json(*)
-    raise NotImplementedError
-  end
-
-  def as_json(*)
-    raise NotImplementedError
-  end
-
   class << self
     def floor(field)
       SQL[:floor][adapter] % field
