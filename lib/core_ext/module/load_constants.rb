cf916e10949db43ce6f2b6f86082b367f04acfcd
Piotr Sarnacki <drogus@gmail.com>
2013-01-19 20:30:09 +0100

    Allow to specify constants to load in load_constants with :only

--- lib/core_ext/module/load_constants.rb
+++ lib/core_ext/module/load_constants.rb
@@ -1,19 +1,24 @@
 class Module
   class Preloader
-    attr_reader :skip_patterns, :skip_names, :debug
+    attr_reader :only_patterns, :only_names, :skip_patterns, :skip_names, :debug
 
     def initialize(options)
       skip = options[:skip] || []
+      only = options[:only] || []
       @debug = options[:debug]
-      @skip_patterns, @skip_names = skip.partition do |skip|
-        skip.is_a?(Regexp)
+
+      partition = lambda do |thing|
+        thing.is_a?(Regexp)
       end
+
+      @skip_patterns, @skip_names = skip.partition &partition
+      @only_patterns, @only_names = only.partition &partition
     end
 
     def load_constants(const)
       const.constants.each do |name|
         full_name = [const.name, name].join('::')
-        unless skip?(full_name)
+        if only?(full_name) && !skip?(full_name)
           skip_names << full_name
           puts "preloading #{full_name}" if debug
           child = begin
@@ -28,6 +33,11 @@ class Module
 
     def skip?(name)
       skip_names.include?(name) || skip_patterns.any? { |pattern| pattern =~ name }
+    end
+
+    def only?(name)
+      (only_names.length == 0 && only_patterns.length == 0) ||
+        only_names.include?(name) || only_patterns.any? { |pattern| pattern =~ name }
     end
 
     def loadable?(const)
