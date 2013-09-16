fddbcabe10ca52abf4fe117d60c839404e5399f8
Sven Fuchs <me@svenfuchs.com>
2013-09-16 16:08:45 +0200

    preserve log formatting for metrics logger reporter

--- lib/travis/support/metrics.rb
+++ lib/travis/support/metrics.rb
@@ -2,22 +2,45 @@ require 'metriks'
 
 module Travis
   module Metrics
+    module Reporter
+      class << self
+        def logger
+          require 'metriks/reporter/logger'
+          logger = Logger.new($stdout)
+          logger.formatter = lambda do |severity, date, progname, message|
+            "#{message}\n"
+          end
+          Metriks::Reporter::Logger.new(
+            logger: logger,
+            on_error: lambda { |error| puts error }
+          )
+        end
+
+        def graphite
+          require 'metriks/reporter/graphite'
+          Metriks::Reporter::Graphite.new
+        end
+      end
+    end
+
     METRICS_VERSION = 'v1'
 
     class << self
+      attr_reader :reporter
+
       def setup(reporter = nil)
         reporter ||= Travis.config.metrics.reporter
         if reporter
           Travis.logger.info("Starting metriks reporter #{reporter}.")
-          require "metriks/reporter/#{reporter}"
-          @reporter = Metriks::Reporter.const_get(reporter.camelize).new.start
+          @reporter = Reporter.send(reporter)
+          @reporter.start
         else
           Travis.logger.info('No metriks reporter configured.')
         end
       end
 
       def started?
-        !!@reporter
+        !!reporter
       end
 
       def meter(event, options = {})
