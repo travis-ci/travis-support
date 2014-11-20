08fd63ba020ba4d819063a7a7bed4342d266582d
Sven Fuchs <me@svenfuchs.com>
2014-11-20 16:20:05 +0100

    use the given channel prefix option in Travis::Amqp::Consumer (march_hare)

--- lib/travis/support/amqp/march_hare/consumer.rb
+++ lib/travis/support/amqp/march_hare/consumer.rb
@@ -47,7 +47,7 @@ module Travis
 
         def channel
           Amqp.connection.create_channel.tap do |channel|
-            channel.prefetch = 1
+            channel.prefetch = options[:channel][:prefetch]
           end
         end
 
---
+++ spec/travis/amqp_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+# ouch, no other tests for amqp at all?
+
+if defined?(JRUBY_VERSION)
+  describe Travis::Amqp::Consumer do
+    describe 'channel prefetch' do
+      let(:consumer)   { described_class.new('queue', options) }
+      let(:queue)      { stub('queue', bind: nil, subscribe: nil) }
+      let(:channel)    { stub('channel', queue: queue, :prefetch= => nil) }
+      let(:connection) { stub('connection', create_channel: channel) }
+
+      before do
+        Travis::Amqp.stubs(:connection).returns(connection)
+      end
+
+      describe 'with no options passed' do
+        let(:options) { {} }
+
+        it 'defaults to 1' do
+          channel.expects(:prefetch=).with(1)
+          consumer.subscribe
+        end
+      end
+
+      describe 'with a prefetch option passed' do
+        let(:options)  { { channel: { prefetch: 2 } } }
+
+        it 'uses the given value' do
+          channel.expects(:prefetch=).with(2)
+          consumer.subscribe
+        end
+      end
+    end
+  end
+end
