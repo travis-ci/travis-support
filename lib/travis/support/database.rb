3ad747571a7c1c5fb65cc88f4333d7540223cd73
Sven Fuchs <me@svenfuchs.com>
2015-09-21 17:18:55 +0200

    directly connect to the db, do not set configuration to ActiveRecord::Base

--- lib/travis/support/database.rb
+++ lib/travis/support/database.rb
@@ -9,16 +9,11 @@ end
 module Travis
   module Database
     class << self
-      def connect(env = nil, config = nil)
-        env    ||= Travis.config.env
-        config ||= Travis.config.database
-
+      def connect(config = nil)
+        ActiveRecord::Base.establish_connection(config || Travis.config.database)
         ActiveRecord::Base.default_timezone = :utc
         ActiveRecord::Base.logger = Travis.logger
-        ActiveRecord::Base.configurations = { env.to_s => config }
-        ActiveRecord::Base.establish_connection(Travis.env.to_sym)
       end
     end
   end
 end
-
